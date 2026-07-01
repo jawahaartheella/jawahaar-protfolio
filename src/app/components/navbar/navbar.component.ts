49a2a161e5234ca057e7413805211d97c22b1ba6
Jawahaar <jawahaartheella@gmail.com>
2026-07-01 11:01:39 +0530

    Added Navbar

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
+
+@Component({
+  selector: 'navbar',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './navbar.component.html'
+})
+export class NavbarComponent {
+
+}
