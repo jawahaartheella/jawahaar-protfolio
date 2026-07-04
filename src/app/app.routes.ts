b189dd5564f0695e16c69cd80eac9f187328b25b
Jawahaar <jawahaartheella@gmail.com>
2026-07-04 11:40:12 +0530

    Made Navbar responsive and added about to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) },
-    { path: 'project', loadComponent: () => import('./pages/project/project.component').then(m => m.ProjectComponent) },
+    { path: 'about', loadComponent: () => import('./pages/about/about.component').then(m => m.AboutComponent) },
+    { path: 'projects', loadComponent: () => import('./pages/project/project.component').then(m => m.ProjectComponent) },
     { path: 'contact', loadComponent: () => import('./pages/contact/contact.component').then(m => m.ContactComponent) },
 ];
