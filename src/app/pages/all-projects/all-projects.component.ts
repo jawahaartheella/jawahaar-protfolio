95708193c91bfc48d20fe897e1d09c78da039cc2
Jawahaar <jawahaartheella@gmail.com>
2026-07-16 09:29:03 +0530

    Added skeleton for project cards

--- src/app/pages/all-projects/all-projects.component.ts
+++ src/app/pages/all-projects/all-projects.component.ts
@@ -5,22 +5,32 @@ import { LucideCodeXml, LucideExternalLink } from '@lucide/angular';
 
 import { Project } from '../../interfaces/project.interface';
 import { ProjectsService } from '../../services/projects.service';
+import { ProjectCardSkeletonComponent } from '../../skeletons/project-card-skeleton/project-card-skeleton.component';
 
 @Component({
-  selector: 'app-all-projects',
-  imports: [LucideCodeXml, LucideExternalLink],
+  selector: 'all-projects',
+  imports: [LucideCodeXml, LucideExternalLink, ProjectCardSkeletonComponent],
   templateUrl: './all-projects.component.html'
 })
 export class AllProjectsComponent {
-  projects= signal<Project[]>([]);
+  projects = signal<Project[]>([]);
+  isLoading = signal(true);
 
   projectService = inject(ProjectsService);
   router = inject(Router);
   route = inject(ActivatedRoute);
 
   ngOnInit() {
-    this.projects.set(this.route.snapshot.data['projects']);
-    console.log(this.projects());
+    this.projectService.getProjectsList().subscribe({
+      next: (res) => {
+        this.projects.set(res);
+        this.isLoading.set(false);
+      },
+      error: (err) => {
+        console.log("Error: ", err.message);
+        this.isLoading.set(true);
+      }
+    })
   }
 
   goToContact() {
---
+++ src/app/skeletons/project-card-skeleton/project-card-skeleton.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'project-card-skeleton',
+  imports: [],
+  templateUrl: './project-card-skeleton.component.html'
+})
+export class ProjectCardSkeletonComponent {
+
+}
