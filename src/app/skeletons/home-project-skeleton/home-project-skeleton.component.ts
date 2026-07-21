cea1bb318c62399cd6ae3ee4632dededd9d34f1f
Jawahaar <jawahaartheella@gmail.com>
2026-07-21 10:49:22 +0530

    Added home project skeleton and other changes

---
+++ src/app/skeletons/home-project-skeleton/home-project-skeleton.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'home-project-skeleton',
+  imports: [],
+  templateUrl: './home-project-skeleton.component.html'
+})
+export class HomeProjectSkeletonComponent {
+
+}
